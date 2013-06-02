d95faaa7bf12c6e1fd5b4d583bf3568fd676c0b9
Prajjwol <enigmaprajjwol2007@gmail.com>
2013-06-02 18:21:35 +0545

    Add the view to add map

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -11,15 +11,25 @@ class Home extends CI_Controller {
         if ($query = $this->cluster_model->get_clusters()) {
             $data['cluster_info'] = $query;
         }
-        $this->load->view('template/template', $data);
+       //$this->load->view('template/template', $data);
+       $this->openlayer();
     }
 
     public function openLayer() {
         $data['main_content'] = "openLayer";
+        if($query = $this->cluster_model->get_cluster_info()){
+            $data['cluster_info'] = $query;
+        }
+        if($query = $this->cluster_model->get_popup_info()){
+            $data['popup_info'] = $query;
+        }
+        // if ($query = $this->cluster_model->get_num_clusters()) {
+        //     $data['num_cluster'] = $query;
+        //  }
        // $data['title'] = "";
+        //echo 'The OpenLayer Page';
         $this->load->view('openLayer', $data);
     }
 
 }
-
 ?>
--- application/models/cluster_model.php
+++ application/models/cluster_model.php
@@ -9,6 +9,16 @@ class Cluster_model extends CI_Model{
 		return $res->result();
 		}
 		}
-	
+	function get_cluster_info(){
+		//This is the format for the list of district to be passed to the view
+		$list= '"DAILEKH,DOLPA","JUMLA,KALIKOT,MUGU","HUMLA,BAJURA,BAJHANG,ACHHAM,DOTI,KAILALI,KANCHANPUR,DADELDHURA,BAITADI,DARCHULA"';
+		return $list;
+	}
+	function get_popup_info(){
+		//This is the format for the list of pop up info for each clusters
+		$popup ='"This is sample popup","This is an another popup","This is another popup"';
+		return $popup;
+	}
+
 	}
 ?>
